1ced6d65969825f5a83f72f4f28b5fcf85240643
c-kinjo <kitakore222@yahoo.co.jp>
2020-04-16 19:29:24 +0900

    botのMessage応答をスレッド内に返すAPI追加

---
+++ python/slack_bot/plugins/botmessage.py
@@ -0,0 +1,29 @@
+# チャンネルかスレッドかどうかで応答先を切り替える
+
+"""
+message.send() の代わりの関数
+  :param messsage: slackbotのmessageオブジェクト
+  :param text    : 送信するテキストメッセージ
+"""
+def botsend(message, text):
+    if 'thread_ts' in message.body:
+        # スレッド内のメッセージの場合
+        message.send(text, thread_ts=message.thread_ts)
+    else:
+        # 親メッセージの場合
+        message.send(text, thread_ts=None)
+
+
+"""
+message.reply() の代わりの関数
+  :param messsage: slackbotのmessageオブジェクト
+  :param text    : 送信するテキストメッセージ
+"""
+def botreply(message, text):
+    if 'thread_ts' in message.body:
+        # スレッド内のメッセージの場合
+        message.reply(text, in_thread=True)
+    else:
+        # 親メッセージの場合
+        message.reply(text)
+

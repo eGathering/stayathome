e87175a5e0cf2a1ded02a0a16c01c097aefa8dec
c-kinjo <kitakore222@yahoo.co.jp>
2020-04-20 17:11:05 +0900

    不具合修正と初めての関数テーブル

--- python/slack_bot/plugins/botmodule.py
+++ python/slack_bot/plugins/botmodule.py
@@ -3,7 +3,7 @@
 bot ちゃんがどんな単語で反応するかを書く
 
 note:
-  検出単語に対して定型文で返す場合は replyDictionary.py をに追加する
+  検出単語に対して定型文で返す場合は replyDictionary.py に追加する
 """
 
 from slackbot.bot import respond_to     # メンションで反応する
@@ -14,90 +14,37 @@ from random import choice
 from random import randrange
 import re
 
-from plugins.botmessage import botSend             # message.send() の代わりの関数
-from plugins.botmessage import botReply            # message.reply() の代わりの関数
-from plugins.replyDictionary import getReplyMsg    # 頭脳(辞書)
-from plugins.replyDictionary import getListenWord  # 頭脳(辞書)
-from .wf import weather                            # 天気予報
+from .botmessage import botSend             # message.send() の代わりの関数
+from .botmessage import botReply            # message.reply() の代わりの関数
+from .replyDictionary import reply_Msg      # 頭脳(辞書)
+from .replyDictionary import listen_Msg     # 頭脳(辞書)
+from .wf import weather                     # 天気予報
 
 #########################################
-# 「respond_to」はメンションする
-#  (@でターゲットを指定すること)と応答する
+# カスタム定型文の関数
 #########################################
 
 
-@respond_to('今何時')
 def get_time_now(message):
-    # 現在の日時を取得
-    dt_now = datetime.now()
-    # メンションつけて応答
+    dt_now = datetime.now()  # 現在の日時を取得
     botReply(message, str(dt_now.hour) + "時" + str(dt_now.minute) + "分だZ！")
 
-@respond_to('天気')
-def weather_forecast(message):
-    text = message.body['text']
-    for line in weather.forecast(text):
-        botReply(message, line)
 
-@respond_to('.')
-def reply_dict_Msg(message):
-    # メッセージを取り出す.
-    text = message.body['text']
-    # 辞書にキーワードがあれば返答する
-    msg = ['none', 'none']
-    getReplyMsg(text, msg)
-    replyMsg = msg[0]
-    type     = msg[1]
-    if 'NoReplyMsg' != replyMsg:
-        if 'Reply' in type:
-            botReply(message, replyMsg)
-        else:
-            botSend(message, replyMsg)
-    else:
-        default_func(message)
-
-#########################################
-# 「listen_to」はメンションがなくても応答する
-#########################################
-
-
-@listen_to('仕事終わり')
 def end_job(message):
     message.react('party_parrot')
     botReply(message, '\n今日も1日お疲れ様でした。\n')
 
 
-@listen_to('今何時')
-def get_time_now(message):
-    # 現在の日時を取得
-    dt_now = datetime.now()
-    # メンションつけずに応答
-    botSend(message, str(dt_now.hour) + "時" + str(dt_now.minute) + "分だZ！")
+def weather_forecast(message):
+    text = message.body['text']
+    for line in weather.forecast(text):
+        botReply(message, line)
 
 
-@listen_to('酒')
 def liquar_func(message):
     drink = ('水', 'お茶', '青汁', 'カレー', 'サーモン')
     response = '\n飲みましょう！\n\n各自...\n自宅で...\n{}などを...'.format(choice(drink))
     botReply(message, response)
-
-
-@listen_to('.')
-def dict_msg(message):
-    # メッセージを取り出す.
-    text = message.body['text']
-    # 辞書にキーワードがあれば返答する
-    msg = ['none', 'none']
-    getListenWord(text, msg)
-    replyMsg = msg[0]
-    type     = msg[1]
-    if 'NoListenWord' != replyMsg:
-        if 'Reply' in type:
-            botReply(message, replyMsg)
-        else:
-            botSend(message, replyMsg)
-    else:
-        de_func(message)
 
 
 def de_func(message):
@@ -125,6 +72,103 @@ def de_func(message):
         return True
     return False
 
+# 関数テーブル
+func_Msg = {
+    # 受けたメッセージ: 関数名
+    '今何時': get_time_now,
+    '仕事終わ': end_job,
+    '天気': weather_forecast,
+    '酒': liquar_func,
+}
+
+"""
+bot ちゃんのカスタム定型文
+"""
+
+
+def request_Func_Msg(message, keyword):
+    for key in func_Msg.keys():
+        if key in keyword:
+            func_Msg[key](message)
+            return True
+    # No Hit
+    return False
+
+#########################################
+# 定型文の関数
+#########################################
+
+
+message_func = {
+    'Reply': botReply,
+    'Send': botSend,
+}
+
+"""
+メンション有りメッセージに定型文を返す
+"""
+
+
+def request_Reply_Msg(message, keyword):
+    for key in reply_Msg.keys():
+        if key in keyword:
+            replyMsg = reply_Msg[key][0]
+            msgType = reply_Msg[key][1]
+            message_func[msgType](message, replyMsg)
+            return True
+    # No Hit
+    return False
+
+"""
+メンション無しメッセージに反応する定型文を返す
+"""
+
+
+def request_ListenWord(message, keyword):
+    for key in listen_Msg.keys():
+        if key in keyword:
+            replyMsg = listen_Msg[key][0]
+            msgType = listen_Msg[key][1]
+            message_func[msgType](message, replyMsg)
+            return
+    # No Hit
+    de_func(message)
+    return
+
+#########################################
+# 「respond_to」はメンションする
+#  (@でターゲットを指定すること)と応答する
+#########################################
+
+
+@respond_to('.')
+def reply_dict_Msg(message):
+    # メッセージを取り出す.
+    text = message.body['text']
+    # 辞書にキーワードがあれば返答する
+    # 関数化辞書
+    if True == request_Func_Msg(message, text):
+        return            # 処理終了
+
+    # 定型文辞書
+    if False == request_Reply_Msg(message, text):
+        default_func(message)
+
+#########################################
+# 「listen_to」はメンションがなくても応答する
+#########################################
+
+
+@listen_to('.')
+def dict_msg(message):
+    # メッセージを取り出す.
+    text = message.body['text']
+    # 辞書にキーワードがあれば返答する
+    # 関数化辞書
+    if False == request_Func_Msg(message, text):
+        # 定型文辞書
+        request_ListenWord(message, text)
+
 #########################################
 # 「default_reply」はデフォルトの応答
 #########################################
@@ -132,7 +176,7 @@ def de_func(message):
 
 @default_reply()
 def default_func(message):
-    # 改行コードされるとDEFAULT_REPLYに流れる。
+    # 改行されるとDEFAULT_REPLYに流れる。
     text = message.body['text']     # メッセージを取り出す.
     msg = 'さん\n' + text + '?\nすみません。言ってる意味がよく分かりません(●´ω｀●)\nで？\n'
     botReply(message, msg)
--- python/slack_bot/plugins/replyDictionary.py
+++ python/slack_bot/plugins/replyDictionary.py
@@ -14,6 +14,7 @@ reply_Msg = {
     '励ます': ['ハゲるんですか？頑張って下さい。', 'Reply'],
     'かわいい': ['あんたバカァ⁈', 'Reply'],
     'おやすみ': ['おやすみなさい。\nまた明日。\n一周回ってまた来週。\n明日が来ない日なんてこないから。\n全部コロナが悪いから。\n', 'Reply'],
+    '名前': ['ボクサッチー、ヨロシクネ', 'Reply'],
 }
 
 # メンション無しで反応するキーワード辞書
@@ -22,35 +23,7 @@ listen_Msg = {
     'NoListenWord': ['NoListenWord', 'Reply'],
     'ツンデレ': ['ツンツンデレデレパッパラパ〜٩( ᐛ )و', 'Reply'],
     'かわいい': ['私のことですね？知ってますよ。', 'Reply'],
-    'botちゃん': ['呼びましたか？', 'Reply'],
+    'botちゃん': ['ボクサッチー、ヨロシクネ', 'Reply'],
     'かしこい': ['かわいい!!エリーチカ!!', 'Send'],
 }
 
-
-"""
-メンション有りメッセージに定型文を返す
-"""
-
-
-def getReplyMsg(keyword, msg):
-    for key in reply_Msg.keys():
-        if key in keyword:
-            msg[0] = reply_Msg[key][0]
-            msg[1] = reply_Msg[key][1]
-            return
-    # No Hit
-    msg[0] = 'NoReplyMsg'
-
-"""
-メンション無しメッセージに反応する定型文を返す
-"""
-
-
-def getListenWord(keyword, msg):
-    for key in listen_Msg.keys():
-        if key in keyword:
-            msg[0] = listen_Msg[key][0]
-            msg[1] = listen_Msg[key][1]
-            return
-    # No Hit
-    msg[0] = 'NoListenWord'
